f76b80c02ae96d07924cb4abd0c86ac2d1117a2a
IrvinA <arevalosirvin@gmail.com>
2021-10-08 01:57:44 -0700

    smurflist.js complete

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Smurf from './Smurf';
 
- const SmurfList = ()=> {
-    const isLoading = false;
+ const SmurfList = (props)=> {
+    const isLoading = props.isLoading;
     const testSmurf = {
         id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
         name:'Poppa Smurf',
@@ -16,11 +17,16 @@ import Smurf from './Smurf';
     }
 
     return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
+        {props.smurfs.map(smurf => (<Smurf smurf={smurf} key={smurf.id}/>))}
     </div>);
 }
 
-export default SmurfList;
+const mapStateToProps = state => ({
+    smurfs: state.smurfs,
+    isLoading: state.isLoading
+});
+
+export default connect(mapStateToProps)(SmurfList);
 
 //Task List:
 //1. Connect the smurfs and loading state values to the SmurfList component.
